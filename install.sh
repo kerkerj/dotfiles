17d6d87cbde855c9d8eba90fa2e58a3872aee89a
kerkerj <jerry54010@gmail.com>
2018-12-11 12:20:51 +0800

    add git related files

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ ln `pwd`/vimrc.before.local ~/.vimrc.before.local
 ln `pwd`/vimrc.bundles.local ~/.vimrc.bundles.local
 ln `pwd`/vimrc.local ~/.vimrc.local
 ln `pwd`/tmux.conf ~/.tmux.conf
+ln `pwd`/.gitignore_global ~/.gitignore_global
+ln `pwd`/.gitconfig ~/.gitconfig
 
 # install
 vim +BundleInstall! +BundleClean +q
