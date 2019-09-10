c5fe87c917aba268661dbc79130df5afd36e99f9
kerkerj <jerry54010@gmail.com>
2019-09-10 14:54:26 +0800

    add scripts for install cmd and softwares

--- install.sh
+++ install.sh
@@ -18,3 +18,9 @@ vim +BundleInstall! +BundleClean +q
 
 # show hidden files
 ./scripts/show_hidden_files.sh
+
+# brew install
+# TODO make a selectable option while executing this script
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install cmake reattach-to-user-namespace ctags-exuberant autojump diff-so-fancy direnv fzf envchain httpie hub jq git git-extras git-flow neovim peco reattach-to-user-namespace the_silver_searcher tig tmux tree wget watch
+brew cask install appcleaner dropbox droplr hyperswitch dash insomnia kekadefaultapp alfred boostnote goland dozer docker spectacle typora zoomus iterm2 launchrocket sourcetree postman
